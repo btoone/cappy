939d4eb3b01d2ff1a3f43b970836d114b0154447
Brandon Toone <btoone@gmail.com>
2011-06-18 18:48:35 -0600

    basic deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,42 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+# the name of your website - should also be the name of the directory
+set :application, "cappy.com"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :repository,  "git@github.com/hinosx/cappy.git"
+set :branch, "master"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+# the name of the deployment user-account on the server
+set :user, "deploy"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# the path to your new deployment directory on the server
+# by default, the name of the application (e.g. "/var/www/sites/example.com")
+set :deploy_to, "/var/www/sites/#{application}"
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+set :scm, :git
+set :keep_releases, 3
+set :use_sudo, false
+
+# Roles
+role :web, "#{application}"
+role :app, "#{application}"
+role :db,  "#{application}"
+
+# Deployment process
+after "deploy:update", "passenger:setup_symlinks"
+after "deploy:migrate"
+
+# Custom deployment tasks
+namespace :passenger do
+  desc "Creates a symlink for the database.yml file"
+  task :setup_symlinks, :roles => :app do
+    puts "\n\n=== Setting up symbolic links ===\n\n"
+    run "ln -s #{deploy_to}/#{shared_dir}/config/database.yml #{current_path}/config/database.yml"
+  end
+end
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
